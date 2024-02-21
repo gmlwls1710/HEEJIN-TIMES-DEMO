e7f1becb93c46c29c845721a424cf6253f88d060
heejin ahn <gmlwls-12@hanmail.net>
2024-02-21 21:02:21 +0900

    third commit

--- main.js
+++ main.js
@@ -1,15 +1,34 @@
 const API_KEY = `1ac0e5f98c57452a834d749b64dde1ec`;
-let news = [];
+let newsList = [];
 
 const getLatestNews = async () => {
   const url = new URL(
     `https://creative-clafoutis-810b22.netlify.app/top-headlines?country=us&apiKey=${API_KEY}`
+    //`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`
   );
   console.log("uuu", url);
   const response = await fetch(url);
   const data = await response.json();
-  news = data.articles;
-  console.log("rrr", news);
+  newsList = data.articles;
+  render();
+  console.log("rrr", newsList);
 };
 
+const render = () => {
+  const newsHTML = newsList
+    .map(
+      (news) => `<div class="row news">
+        <div class="col-lg-4">
+          <img class="news-img-size" src=${news.urlToImage}></img>
+        </div>
+        <div class="col-lg-8">
+          <h2>${news.title}</h2>
+          <p>${news.description}</p>
+          <div>${news.source.name}  * ${news.publishedAt}</div>
+        </div>
+      </div>`
+    )
+    .join("");
+  document.getElementById("news-board").innerHTML = newsHTML;
+};
 getLatestNews();
